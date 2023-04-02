f64381c00a73616d899aea7a50e7f6ff51a441c1
Александр Бирюков <hdnhdxv@gmail.com>
2023-04-02 23:47:06 +0400

    dq

--- Wpf_core_diploma/Pages/pgAddUsers.xaml.cs
+++ Wpf_core_diploma/Pages/pgAddUsers.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DIPloma.DataBase;
+using DIPloma.DataBase.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,34 @@ namespace DIPloma.Pages
         public pgAddUsers()
         {
             InitializeComponent();
+            select();
+        }
+        void select()
+        {
+            IEnumerable<User> listUsers = EfModels.init().Users.Include(p=>p.RoleNavigation).ToList(); 
+            listUsers = listUsers.DistinctBy(p=>p.IdUserss);
+            dgUserMember.ItemsSource = listUsers;
+        }
+
+        private void dgUserMember_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void clChang(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clDel(object sender, RoutedEventArgs e)
+        {
+            User Del = (sender as Button).DataContext as User;
+            if (MessageBox.Show("точно да?", "dqw", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                EfModels.init().Users.Remove(Del);
+                EfModels.init().SaveChanges();
+            }
+            select();
         }
     }
 }
