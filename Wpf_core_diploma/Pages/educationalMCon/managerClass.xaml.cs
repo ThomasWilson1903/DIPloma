2cf32115fe950653b29e69e12d2a4ba1dcd84778
Александр Бирюков <hdnhdxv@gmail.com>
2023-06-02 22:42:48 +0400

    SS

---
+++ Wpf_core_diploma/Pages/educationalMCon/managerClass.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DIPloma.Pages.educationalMCon
+{
+    /// <summary>
+    /// Interaction logic for managerClass.xaml
+    /// </summary>
+    public partial class managerClass : Page
+    {
+        public managerClass()
+        {
+            InitializeComponent();
+        }
+    }
+}
